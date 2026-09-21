a4c28481ee692e091b445b2a9953e1c94a763a7b
saraehrari <saraehrari2020@gmail.com>
2026-09-21 11:39:54 +0430

    Create Navbar.jsx
    Signed-off-by: saraehrari <saraehrari2020@gmail.com>

---
+++ Router-React/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from "react-router-dom";
+
+const linkStyle = ({ isActive }) => ({
+  marginRight: 12,
+  textDecoration: "none",
+  fontWeight: isActive ? "bold" : "normal",
+});
+
+function Navbar({ isAuth, onLogout }) {
+  return (
+    <div style={{ padding: 12, borderBottom: "1px solid #ddd" }}>
+      <NavLink to="/" style={linkStyle}>
+        Home
+      </NavLink>
+
+      <NavLink to="/products" style={linkStyle}>
+        Products
+      </NavLink>
+
+      <NavLink to="/checkout" style={linkStyle}>
+        Checkout
+      </NavLink>
+
+      <span style={{ marginLeft: 20 }}>
+        {isAuth ? (
+          <button onClick={onLogout}>Logout</button>
+        ) : (
+          <span style={{ opacity: 0.7 }}>Not logged in</span>
+        )}
+      </span>
+    </div>
+  );
+}
+
+export default Navbar;
